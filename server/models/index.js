d4eb42975b0fdf32f0c0562d9206e6c90b75ebd5
PluemEDDI <Pluem.p@oventure-group.com>
2026-02-18 15:57:22 +0700

    Link Gmail

--- server/models/index.js
+++ server/models/index.js
@@ -2,3 +2,4 @@ export { default as User } from './User.js';
 export { default as Player } from './Player.js';
 export { default as Group } from './Group.js';
 export { default as Schedule } from './Schedule.js';
+export { default as Invitation } from './Invitation.js';
